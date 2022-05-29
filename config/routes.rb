a46b37470dc82f98500971f5cc9a495a55bf9565
Sergio P. Pino G <patricio.pino@gmail.com>
2022-05-29 19:51:20 -0400

    segundo commit con imagen y controlador

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/one'
+  get 'pages/two'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "pages#one"
+
 end
